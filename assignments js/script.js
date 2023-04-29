ba60099353b14be271526f83ed9adf108cf10acf
deejaybonny <117898562+deejaybonny@users.noreply.github.com>
2023-04-29 20:33:28 +0300

    Add files via upload

---
+++ assignments js/script.js
@@ -0,0 +1,122 @@
+/*
+let js = "amizing";
+      console.log(40 + 8 + 23 - 11);
+
+console.log("Bonny");
+console.log(24);
+
+let firstName = "Bonny";
+let first = "Jonas";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_Bonny = "JB";
+let $function = 25;
+
+let myFirstJob = "programmer";
+let myCurrentJob = "Desinger";
+
+let job1 = "programmer";
+let job2 = "Desinger";
+
+console.log(myFirstJob);
+
+
+
+// console.log(true);
+// let JavascriptIsFun = true;
+
+let JavascriptIsFun = true;
+console.log(true);
+
+// console.log(typeof true);
+console.log(typeof JavascriptIsFun);
+// console.log(typeof 24);
+// console.log(typeof "Bonny");
+
+JavascriptIsFun = "YES!"
+console.log(typeof JavascriptIsFun);
+
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1999;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+
+
+
+/*LECTURE: Values and Variables
+1. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+2. Log their values to the console
+*/
+/*
+let country = "kenya";
+let continent = "africa";
+let population = 53000001;
+
+console.log(country, continent, population);
+
+let language = `swahili`;
+
+population / 2 ; 53000001 / 2 ; 26500000.5
+
+population++; //increase population by 1
+console.log ("new population:", population);
+
+let finlandPopulation= 6000000;
+let usPopulation = 53000002;
+
+if (usPopulation > finlandPopulation) {
+    console.log ("Kenya has more people than finland.");
+} else {
+    console.log ("finland has more people than kenya.");}
+
+    let averagePopulation = 33000000;
+    let kenyaPopulation = 53000002;
+
+
+if (averagePopulation > kenyaPopulation) {
+    console.log ("kenya has less people than the avarange country.");
+} else{
+    console.log ("kenya has more people than the avarange country.");
+}*/
+
+let country = "Portugal";
+let continent = "Europe";
+let population = 11000000;
+
+console.log(country, continent, population);
+
+//let language = `swahili`;
+
+population / 2 ; 11000000 / 2 ; 5500000
+
+population++; //increase population by 1
+console.log ("new population:", population);
+
+let finlandPopulation= 6000000;
+let usPopulation = 11000001;
+
+if (usPopulation > finlandPopulation) {
+    console.log ("Pourtugal has more people than finland.");
+} else {
+    console.log ("Pourtugal has more people than kenya.");}
+
+    let averagePopulation = 33000000;
+    let PourtugalPopulation = 11000001;
+
+
+if (averagePopulation > PourtugalPopulation) {
+    console.log ("Pourtugal has less people than the avarange country.");
+} else{
+    console.log ("Pourtugal has more people than the avarange country."); 
+}
+
